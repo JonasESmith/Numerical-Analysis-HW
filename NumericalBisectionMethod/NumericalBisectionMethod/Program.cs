c557419a7dce8f1efa87de088dd5951ff0f16533
Jonas <pictofoster@gmail.com>
2018-08-29 11:48:54 -0500

    Adding logic for finding midpoint and deciding which way to lean towards convergence

--- NumericalBisectionMethod/NumericalBisectionMethod/Program.cs
+++ NumericalBisectionMethod/NumericalBisectionMethod/Program.cs
@@ -1,15 +1,16 @@
-﻿/// <summary>
+﻿
+using System;
+/// <summary>
 ///     Programmer : Jonas Smith
 ///     Date start : 8/29/18
 ///     Purpose    : Create program that uses bisectin method to find zero between points
 /// </summary>
-
 namespace NumericalBisectionMethod
 {
   class Program
   {
-    const double str_X = 0.5;
-    const double end_X = 4.5;
+    static double str_X = 0.5;
+    static double end_X = 4.5;
 
     static double iterations = 15; 
     
@@ -17,10 +18,28 @@ namespace NumericalBisectionMethod
 
     static void Main(string[] args)
     {
+      double pointOne = 0;
+
       for(int index = 0; index < iterations; index++)
       {
+        pointOne = (str_X + end_X) / 2;
+        if(function(pointOne) > function(str_X))
+        {
 
+        }
+        else
+        {
+          end_X = pointOne;
+        }
+        // if (pointOne is negative then we need to take numbers to the right or left)
       }
+    }
+
+    static double function(double xValue)
+    {
+      double yValue = Math.Pow((xValue - 2), 2) - Math.Log(xValue);
+
+      return yValue;
     }
   }
 }
