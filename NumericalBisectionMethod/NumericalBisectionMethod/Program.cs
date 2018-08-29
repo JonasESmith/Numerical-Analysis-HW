042c3ceed8e1ded32107e15f53d54c920acb62d7
Jonas <pictofoster@gmail.com>
2018-08-29 11:35:27 -0500

    Changing project to be console application

---
+++ NumericalBisectionMethod/NumericalBisectionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+///     Programmer : Jonas Smith
+///     Date start : 8/29/18
+///     Purpose    : Create program that uses bisectin method to find zero between points
+/// </summary>
+
+namespace NumericalBisectionMethod
+{
+  class Program
+  {
+    const double str_X = 0.5;
+    const double end_X = 4.5;
+
+    static double iterations = 15; 
+    
+    // Initial function f(x) = (x-2)^2 - ln(x) = 0; 
+
+    static void Main(string[] args)
+    {
+      for(int index = 0; index < iterations; index++)
+      {
+
+      }
+    }
+  }
+}
