ac3420d995137c6d444b8aec54d2cfb76f7624c9
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-09-03 12:24:58 -0500

    Adding base logic before testing

--- NumericalBisectionMethod/NumericalBisectionMethod/Program.cs
+++ NumericalBisectionMethod/NumericalBisectionMethod/Program.cs
@@ -7,39 +7,59 @@ using System;
 /// </summary>
 namespace NumericalBisectionMethod
 {
-  class Program
-  {
-    static double str_X = 0.5;
-    static double end_X = 4.5;
-
-    static double iterations = 15; 
-    
-    // Initial function f(x) = (x-2)^2 - ln(x) = 0; 
-
-    static void Main(string[] args)
+    class Program
     {
-      double mindPoint = 0;
+        static double start_X = 0.5;
+        static double end_X = 4.5;
 
-      for(int index = 0; index < iterations; index++)
-      {
-        mindPoint = (str_X + end_X) / 2;
-        if(function(mindPoint) > function(str_X))
+        static double iterations = 15;
+
+        // Initial function f(x) = (x-2)^2 - ln(x) = 0; 
+        static void Main(string[] args)
         {
-
+            bool iterateValue = CheckSigns(start_X, end_X);
+            IterateSet(iterateValue);
         }
-        else
+
+        static void IterateSet(bool iterateValue)
         {
-          end_X = mindPoint;
+            double mindPoint = 0;
+
+            if(iterateValue)
+            {
+
+                for (int index = 0; index < iterations; index++)
+                {
+                    mindPoint = (start_X + end_X) / 2;
+                    if (function(mindPoint) > function(start_X))
+                    {
+
+                    }
+                    else
+                    {
+                        end_X = mindPoint;
+                    }
+                    // if (pointOne is negative then we need to take numbers to the right or left)
+                }
+            }
         }
-        // if (pointOne is negative then we need to take numbers to the right or left)
-      }
-    }
 
-    static double function(double xValue)
-    {
-      double yValue = Math.Pow((xValue - 2), 2) - Math.Log(xValue);
+        static bool CheckSigns(double start_X, double end_x)
+        {
+            bool returnValue = true;
 
-      return yValue;
+            if(Math.Sign(start_X) != Math.Sign(end_X))
+            {
+                returnValue = false;
+            }
+            return returnValue;
+        }
+
+        static double function(double xValue)
+        {
+            double yValue = Math.Pow((xValue - 2), 2) - Math.Log(xValue);
+
+            return yValue;
+        }
     }
-  }
 }
