93cc05451b15e6e1dbdeef006a548af3161b6570
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-10-21 11:44:04 -0500

    Adding NumericalHW3 and the main algorithms that will be used

---
+++ NumericHW/NumericalHW3/Program.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+///     Programmer :  Jonas Smith
+///     Purpose    :  Use the three algorithms from the text found on page 213
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalHW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static double Function(double value, int iteration)
+        {
+            double returnValue = 0.00;
+
+            for(int i = 0; i < (iteration - 1); i++)
+            {
+
+            }
+
+            return returnValue;
+        }
+
+        static double Derivative(double value)
+        {
+            double returnValue = 0.00;
+
+            return returnValue;
+        }
+
+        static double Integral(double value)
+        {
+            double returnValue = 0.00;
+
+            return returnValue;
+        }
+    }
+}
