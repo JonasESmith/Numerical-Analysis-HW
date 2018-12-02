ee431f0a1456280262700bfc1e1a260d47b8e208
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-12-02 16:19:23 -0600

    adding the file for NumericalHW6

---
+++ NumericHW/NumericalHW6/NumericalHW6.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+///     Programmer : Jonas Smith
+///     Pupose     : Approximate integral using trapezoidal and simpsons rule. 
+/// </summary>
+
+using System;
+
+namespace NumericalHW6
+{
+    class NumericalHW6
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
