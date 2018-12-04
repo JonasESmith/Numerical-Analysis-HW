f04a20c7f8c10432e7be98499a09e3860bd7f47a
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-12-03 21:08:44 -0600

    Adding minor changes to trapezoidal rule

--- NumericHW/NumericalHW6/NumericalHW6.cs
+++ NumericHW/NumericalHW6/NumericalHW6.cs
@@ -8,26 +8,37 @@ namespace NumericalHW6
 {
     class NumericalHW6
     {
-        int[]  subIntervals_M = new int[] { 2, 4, 8, 16, 32 };
+        static int[]  subIntervals_M = new int[] { 2, 4, 8, 16, 32 };
 
-        public void Main()
+        static double actual_Value = (Math.PI / 2) +( Math.Pow(Math.PI, 2) / 4) - 2;
+
+        static void Main()
         {
-            double upperBound     = Math.PI;
+            double upperBound     = Math.PI / 2;
             double lowerBound     = 0.0;
             double nodeSpacing_H  = 0.0;
+
+            double trapezoidValue = 0;
+            double simpsonsValue = 0;
+
+            printHeaders();
 
             for (int index = 0; index < subIntervals_M.Length; index++)
             {
                 nodeSpacing_H  = ( upperBound - lowerBound ) / subIntervals_M[index];
-                TrapezoidalRule(nodeSpacing_H, lowerBound + (index * nodeSpacing_H), index);
+                trapezoidValue = TrapezoidalRule(nodeSpacing_H, lowerBound, upperBound, index);
 
                 nodeSpacing_H = ( upperBound - lowerBound ) / ( subIntervals_M[index] * 2 );
-                SimpsonsRule(nodeSpacing_H, lowerBound, upperBound, index);
+                simpsonsValue = SimpsonsRule(nodeSpacing_H, lowerBound, upperBound, index);
+
+                PrintResults(subIntervals_M[index], trapezoidValue, simpsonsValue, 123.00);
             }
+
+            Console.ReadLine();
         }
 
         // function for both composite methods bellow. 
-        public double function(double x_value)
+        static public double function(double x_value)
         {
             return ((Math.Pow(x_value, 2) + x_value + 1 ) * Math.Cos(x_value));
         }
@@ -36,57 +47,63 @@ namespace NumericalHW6
 
 
         // Composite Trapezoidal Rule - Page 357 1c
-        public double TrapezoidalRule(double nodeSpaceH, double x_value, int index)
+        static public double TrapezoidalRule(double H, double lowerBound, double upperBound, int M)
         {
-            return (nodeSpaceH / 2) *( function(x_value) + function(x_value + nodeSpaceH))
-                                    + nodeSpaceH * TrapezoidalError(nodeSpaceH, x_value, index);
+            return (H / 2) * ( function(lowerBound) + function(upperBound) )
+                                    + TrapezoidalError(H, lowerBound, M);
         }
 
-        public double TrapezoidalError(double nodeSpaceH, double x_value, int index) {
+        static public double TrapezoidalError(double H, double lowerBound, int M) {
             double functionSummation = 0.0;
 
-            for(int i = 1; i <= subIntervals_M[index] - 1; i++)
+            for(int i = 1; i <= subIntervals_M[M] - 1; i++)
             {
-                functionSummation += function(x_value + (i * nodeSpaceH) );
+                functionSummation += function(lowerBound + (i * H) );
             }
 
-            return nodeSpaceH * functionSummation;
+            return H * functionSummation;
         }
 
 
 
 
         // Composite Simpson's Rule - Page 359 4c
-        public double SimpsonsRule(double nodeSpaceH, double lowerBound, double upperBound, int index)
+        static public double SimpsonsRule(double H, double lowerBound, double upperBound, int M)
         {
-            return (nodeSpaceH / 3) * (function(lowerBound) + function(upperBound))
-                                    + SimpsonsError(nodeSpaceH , lowerBound, index);
+            return (H / 3) * (function(lowerBound) + function(upperBound))
+                                    + SimpsonsError(H , lowerBound, M);
         }
 
-        public double SimpsonsError(double nodeSpaceH,double lowerBound, int index)
+        static public double SimpsonsError(double H,double lowerBound, int index)
         {
             double firstSummation  = 0.0;
             double secondSummation = 0.0;
 
             for(int i = 1; i <= subIntervals_M[index] - 1; i++)
             {
-                firstSummation += function(lowerBound + ((2 * i) * nodeSpaceH));
+                firstSummation += function(lowerBound + ((2 * i) * H));
             }
 
             for(int i = 1; i <= subIntervals_M[index]; i++)
             {
-                secondSummation += function(lowerBound + ((2 * i - 1) * nodeSpaceH));
+                secondSummation += function(lowerBound + ((2 * i - 1) * H));
             }
 
-            return (2 * nodeSpaceH) / 3 * (firstSummation) + ((4 * nodeSpaceH) / 3) * secondSummation;
+            return (2 * H) / 3 * (firstSummation) + ((4 * H) / 3) * secondSummation;
         }
 
 
-
+        static void printHeaders()
+        {
+            Console.WriteLine("|----|--------------|--------------|----------|");
+            Console.WriteLine("|                                             |");
+            Console.WriteLine("|----|--------------|--------------|----------|");
+        }
 
         // prints the results for the methods used above and each interval. 
-        public void PrintResults()
+        static public void PrintResults(int mSub_value, double trapezoidalValue, double simpsonsValue, double errorValue)
         {
+            Console.WriteLine("|{0,3} | {1,12} | {2,12} | {3,8} |", mSub_value, String.Format("{0:N10}", trapezoidalValue), String.Format("{0:N10}", simpsonsValue), errorValue);
             // print results in the form of SubInterValM[i], Trapezoidal, Simpson's, Error. 
         }
 
